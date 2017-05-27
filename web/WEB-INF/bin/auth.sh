2587c4c178a4b551535e7915b92c8ae262459e70
Claas Nieslony <bb@nieslony.at>
2017-05-27 12:51:35 +0200

    Fix: always return 0 :-(

--- web/WEB-INF/bin/auth.sh
+++ web/WEB-INF/bin/auth.sh
@@ -4,7 +4,6 @@ PWD_FILE=$2
 AUTH_URL=$1
 MACHINE=$( echo $AUTH_URL | sed -e 's/^https\?:\/\/\([a-z.\-]\+\).*/\1/g' )
 
-(
 echo "--- BEGIN authentication script ---"
 echo "Reading username and password from $PWD_FILE"
 echo "Trying to get URL $AUTH_URL ..."
@@ -25,7 +24,8 @@ else
     echo Authentication failed.
     RET=1
 fi
+
+echo Exit with code $RET
 echo "--- END authentication script ---"
-) | logger
 
 exit $RET
