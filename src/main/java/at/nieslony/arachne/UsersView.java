deb787445643e3a890b696eaf90e84d74b8129fa
Claas Nieslony <bb@nieslony.at>
2023-03-01 00:33:20 +0100

    Make addUser dialog work

--- src/main/java/at/nieslony/arachne/UsersView.java
+++ src/main/java/at/nieslony/arachne/UsersView.java
@@ -7,21 +7,21 @@ package at.nieslony.arachne;
 import at.nieslony.arachne.roles.RolesCollector;
 import at.nieslony.arachne.users.ArachneUser;
 import at.nieslony.arachne.users.UserRepository;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.editor.Editor;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
-import com.vaadin.flow.data.validator.EmailValidator;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 import javax.annotation.security.RolesAllowed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,107 +40,114 @@ public class UsersView extends VerticalLayout {
     final private UserRepository userRepository;
     final private RolesCollector rolesCollector;
 
+    final Grid<ArachneUser> usersGrid;
+
     public UsersView(UserRepository userRepository, RolesCollector rolesCollector) {
         this.userRepository = userRepository;
         this.rolesCollector = rolesCollector;
 
-        Grid<ArachneUser> grid = new Grid<>(ArachneUser.class, false);
-        Editor<ArachneUser> editor = grid.getEditor();
-        Label usernameStatus = new Label();
-        Label displayNameStatus = new Label();
-        Label emailStatus = new Label();
+        usersGrid = new Grid<>(ArachneUser.class, false);
 
-        Grid.Column<ArachneUser> usernameColumn = grid
+        Button addUserButton = new Button("Add User...",
+                event -> addUser()
+        );
+
+        Grid.Column<ArachneUser> usernameColumn = usersGrid
                 .addColumn(ArachneUser::getUsername)
                 .setHeader("Username");
-        Grid.Column<ArachneUser> displayNameColumn = grid
+        Grid.Column<ArachneUser> displayNameColumn = usersGrid
                 .addColumn(ArachneUser::getDisplayName)
                 .setHeader("Displayname");
-        Grid.Column<ArachneUser> emailColumn = grid
+        Grid.Column<ArachneUser> emailColumn = usersGrid
                 .addColumn(ArachneUser::getEmail)
                 .setHeader("E-Mail");
-        /*grid.addComponentColumn((user) -> {
+        usersGrid.addComponentColumn((user) -> {
             String roles = rolesCollector
                     .findRoleDescriptionsForUser(user.getUsername())
                     .stream()
                     .collect(Collectors.joining(", "));
             return new Text(roles);
-        }).setHeader("Roles");*/
-        Grid.Column<ArachneUser> editColumn = grid
-                .addComponentColumn((user) -> {
-                    Button editButton = new Button("Edit");
-                    editButton.addClickListener((t) -> {
-                        if (editor.isOpen()) {
-                            editor.cancel();
-                        }
-                        grid.getEditor().editItem(user);
-                    });
-                    return editButton;
-                });
-        Binder<ArachneUser> binder = new Binder<>(ArachneUser.class);
-        editor.setBinder(binder);
-        editor.setBuffered(true);
-
-        TextField usernameField = new TextField();
-        usernameField.setWidthFull();
-        binder.forField(usernameField)
-                .asRequired("Username is required")
-                .withStatusLabel(usernameStatus)
-                .bind(ArachneUser::getUsername, ArachneUser::setUsername);
-        usernameColumn.setEditorComponent(usernameField);
-
-        TextField displayNameField = new TextField();
-        displayNameField.setWidthFull();
-        binder.forField(displayNameField)
-                .withStatusLabel(displayNameStatus)
-                .bind("displayName");
-        displayNameColumn.setEditorComponent(displayNameField);
-
-        EmailField emailField = new EmailField();
-        emailField.setWidthFull();
-        binder.forField(emailField)
-                .withValidator(new EmailValidator(
-                        "Please enter valid email address",
-                        true))
-                .withStatusLabel(emailStatus)
-                .bind(ArachneUser::getEmail, ArachneUser::setEmail);
-        emailColumn.setEditorComponent(emailField);
-
-        Button saveButton = new Button("Save",
-                e -> {
-                    if (editor.save()) {
-                        logger.info("User saved");
-                        grid.getEditor().closeEditor();
-                    } else {
-                        logger.error("Error saving user");
-                    }
-                });
-        Button cancelButton = new Button(VaadinIcon.CLOSE.create(),
-                e -> {
-                    editor.cancel();
-                });
-        cancelButton.addThemeVariants(
-                ButtonVariant.LUMO_ICON,
-                ButtonVariant.LUMO_ERROR);
-        HorizontalLayout actions = new HorizontalLayout(
-                saveButton,
-                cancelButton
-        );
-        actions.setPadding(false);
-        editColumn.setEditorComponent(actions);
-
-        editor.addCancelListener(e -> {
-            usernameStatus.setText("");
-            displayNameStatus.setText("");
-            emailStatus.setText("");
-        });
+        }).setHeader("Roles");
 
         List<ArachneUser> users = userRepository.findAll();
-        grid.setItems(users);
+        usersGrid.setItems(users);
 
-        add(grid,
-                usernameStatus,
-                displayNameStatus,
-                emailStatus);
+        add(addUserButton, usersGrid);
+    }
+
+    void addUser() {
+        Dialog dialog = new Dialog();
+        dialog.setHeaderTitle("Add User");
+        Binder<ArachneUser> binder = new Binder(ArachneUser.class);
+
+        Button okButton = new Button("OK",
+                event -> {
+                    ArachneUser newUser = new ArachneUser();
+                    if (binder.writeBeanIfValid(newUser)) {
+                        userRepository.save(newUser);
+                        dialog.close();
+
+                        usersGrid.setItems(userRepository.findAll());
+                    }
+                });
+        okButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        Button cancelButton = new Button("Cancel",
+                event -> {
+                    dialog.close();
+                });
+        dialog.getFooter().add(cancelButton, okButton);
+
+        TextField usernameField = new TextField("Username");
+        usernameField.setValue("");
+        TextField displayNameField = new TextField("Display Name");
+        PasswordField passwordField = new PasswordField("Password");
+        PasswordField retypePasswordField = new PasswordField("Retype Password");
+
+        binder.addStatusChangeListener((event) -> {
+            okButton.setEnabled(!event.hasValidationErrors());
+        });
+
+        binder.forField(usernameField)
+                .asRequired()
+                .withValidator(username -> {
+                    return username.matches("^[a-z].*");
+                }, "Username must start with lowercase letter")
+                .withValidator(username -> {
+                    return username.matches("^[a-z0-9_.\\-]+$");
+                }, "Allowed characters: a-z 0-9 - . -")
+                .withValidator(username -> {
+                    ArachneUser user = userRepository.findByUsername(username);
+                    return user == null;
+                }, "User already exists")
+                .bind(ArachneUser::getUsername, ArachneUser::setUsername);
+        binder.forField(displayNameField)
+                .bind(ArachneUser::getDisplayName, ArachneUser::setDisplayName);
+        binder.forField(passwordField)
+                .bind(ArachneUser::getPassword, ArachneUser::setPassword);
+        AtomicReference<String> retypePasswordStr = new AtomicReference<>("");
+        binder.forField(retypePasswordField)
+                .withValidator(
+                        retypePassword -> {
+                            String password = passwordField.getValue();
+                            return password.equals(retypePassword);
+                        },
+                        "Passwords don't match")
+                .bind((source) -> retypePasswordStr.get(), (bean, fieldvalue) -> {
+                    retypePasswordStr.set(fieldvalue);
+                });
+
+        passwordField.addValueChangeListener((event) -> {
+            binder.validate();
+        });
+
+        binder.validate();
+
+        dialog.add(new FormLayout(
+                usernameField,
+                displayNameField,
+                passwordField,
+                retypePasswordField));
+
+        dialog.open();
     }
 }
