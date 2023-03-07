36f143d127062c7450664e8527bb842af56f6b9a
Claas Nieslony <bb@nieslony.at>
2023-03-07 13:20:40 +0100

    Add IpValidator.java

---
+++ src/main/java/at/nieslony/arachne/IpValidator.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package at.nieslony.arachne;
+
+import com.vaadin.flow.function.SerializablePredicate;
+
+/**
+ *
+ * @author claas
+ */
+class IpValidator implements SerializablePredicate<String> {
+
+    @Override
+    public boolean test(String value) {
+        if (value == null || value.equals("")) {
+            return true;
+        }
+
+        String[] bytes = value.split("\\.");
+        if (bytes.length != 4) {
+            return false;
+        }
+        try {
+            for (String b : bytes) {
+                int intVal = Integer.parseInt(b);
+                if (intVal < 0 || intVal > 255) {
+                    return false;
+                }
+            }
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
