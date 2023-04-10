eb399731d5aea18882893820dbc4aa61b817b8da
Claas Nieslony <bb@nieslony.at>
2023-04-10 19:24:53 +0200

    Add AuthRestController.java

---
+++ src/main/java/at/nieslony/arachne/AuthRestController.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2023 claas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.nieslony.arachne;
+
+import jakarta.annotation.security.RolesAllowed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author claas
+ */
+@RestController
+public class AuthRestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthRestController.class);
+
+    @GetMapping("/api/auth")
+    @RolesAllowed(value = {"USER"})
+    public String auth() {
+        logger.info("User authenticated for VPN access");
+        return "Authenticated";
+    }
+}
