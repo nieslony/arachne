c45bebb760999ec1a93895454d26972b1a7d5168
Claas Nieslony <bb@nieslony.at>
2023-06-07 12:45:18 +0200

    Add ConditionalValidator.java and IgnoringInvisibleValidator.java

---
+++ src/main/java/at/nieslony/arachne/utils/IgnoringInvisibleValidator.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package at.nieslony.arachne.utils;
+
+import com.vaadin.flow.data.binder.ValidationResult;
+import com.vaadin.flow.data.binder.Validator;
+import com.vaadin.flow.data.binder.ValueContext;
+
+/**
+ *
+ * @author claas
+ * @param <Value>
+ */
+public class IgnoringInvisibleValidator<Value> implements Validator<Value> {
+
+    private final Validator<Value> validator;
+
+    public IgnoringInvisibleValidator(Validator<Value> validator) {
+        this.validator = validator;
+    }
+
+    @Override
+    public ValidationResult apply(Value t, ValueContext vc) {
+        if (!vc.getComponent().get().isVisible()) {
+            return ValidationResult.ok();
+        } else {
+            return validator.apply(t, vc);
+        }
+    }
+}
