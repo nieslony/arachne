d08e4f4d4ce427d4bf4010b4b19617b4cacfc0b2
Claas Nieslony <bb@nieslony.at>
2023-06-29 14:37:34 +0200

    Show connected users

--- src/main/java/at/nieslony/arachne/MainView.java
+++ src/main/java/at/nieslony/arachne/MainView.java
@@ -4,11 +4,18 @@
  */
 package at.nieslony.arachne;
 
-import com.vaadin.flow.component.Text;
+import at.nieslony.arachne.openvpnmanagement.ConnectedClient;
+import at.nieslony.arachne.openvpnmanagement.OpenVpnManagement;
+import at.nieslony.arachne.openvpnmanagement.OpenVpnManagementException;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.grid.ColumnTextAlign;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.PermitAll;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -19,9 +26,42 @@ import jakarta.annotation.security.PermitAll;
 @PermitAll
 public class MainView extends VerticalLayout {
 
-    public MainView() {
-        Text text = new Text("Home");
+    private static final Logger logger = LoggerFactory.getLogger(MainView.class);
 
-        add(text);
+    private final OpenVpnManagement openVpnManagement;
+
+    public MainView(OpenVpnManagement openVpnManagement) {
+        this.openVpnManagement = openVpnManagement;
+
+        add(createConnectedUsersView());
+    }
+
+    private Component createConnectedUsersView() {
+        VerticalLayout layout = new VerticalLayout();
+        Grid<ConnectedClient> grid = new Grid<>();
+        grid.addColumn(ConnectedClient::getUsername)
+                .setHeader("Username");
+        grid.addColumn(ConnectedClient::getBytesReceived)
+                .setHeader("Bytes Received")
+                .setTextAlign(ColumnTextAlign.END);
+        grid.addColumn(ConnectedClient::getBytesSent)
+                .setHeader("Bytes Sent")
+                .setTextAlign(ColumnTextAlign.END);
+        grid.addColumn(ConnectedClient::getConnectedSince)
+                .setHeader("Connected since");
+        grid.addColumn(ConnectedClient::getRealAddress)
+                .setHeader("Real Address");
+        grid.addColumn(ConnectedClient::getVirtualAddress)
+                .setHeader("Virtual Address");
+
+        try {
+            grid.setItems(openVpnManagement.getConnectedUsers());
+        } catch (OpenVpnManagementException ex) {
+            logger.error(ex.getMessage());
+        }
+
+        layout.add(grid);
+
+        return layout;
     }
 }
