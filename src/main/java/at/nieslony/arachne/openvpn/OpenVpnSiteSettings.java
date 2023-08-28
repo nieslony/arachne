430eeda2620137e02b0e0cc01d0e6fcff6b38e5d
Claas Nieslony <github@nieslony.at>
2023-08-29 00:04:34 +0200

    Add more settings

--- src/main/java/at/nieslony/arachne/openvpn/OpenVpnSiteSettings.java
+++ src/main/java/at/nieslony/arachne/openvpn/OpenVpnSiteSettings.java
@@ -4,7 +4,9 @@
  */
 package at.nieslony.arachne.openvpn;
 
-import at.nieslony.arachne.utils.TransportProtocol;
+import at.nieslony.arachne.settings.Settings;
+import at.nieslony.arachne.utils.net.NetUtils;
+import at.nieslony.arachne.utils.net.TransportProtocol;
 import java.util.Comparator;
 import java.util.List;
 import lombok.Getter;
@@ -22,7 +24,6 @@ public class OpenVpnSiteSettings {
 
     @Getter
     @Setter
-    @ToString
     public class VpnSite {
 
         private int id;
@@ -35,13 +36,69 @@ public class OpenVpnSiteSettings {
         private VpnSite(int id) {
             this.id = id;
         }
+
+        @Override
+        public String toString() {
+            return name;
+        }
     }
 
     private String listenIp;
     private int listenPort;
     private TransportProtocol listenProtocol;
-    private boolean allowConfigureIpOnly;
+    private Boolean mtuTest;
+    private String remote;
+    private String deviceType;
+    private String deviceName;
+    private String clientNetwork;
+    private Integer clientMask;
+    private Integer keepaliveTimeout;
+    private Integer keepaliveInterval;
+
     private List<VpnSite> vpnSites;
+
+    private static final String SK_OPENVPN_SITE_LISTEN_IP = "openvpn.site.listen-ip";
+    private static final String SK_OPENVPN_SITE_LISTEN_PORT = "openvpn.site.listen-port";
+    private static final String SK_OPENVPN_SITE_LISTEN_PROTOCOL = "openvpn.site.listen-protocol";
+    private static final String SK_OPENVPN_SITE_MTU_TEST = "openvpn.site.mtu-test";
+    private static final String SK_OPENVPN_SITE_REMOTE = "openvpn.site.remote";
+    private static final String SK_OPENVPN_SITE_DEVICE_TYPE = "openvpn.site.deviceType";
+    private static final String SK_OPENVPN_SITE_DEVICE_NAME = "openvpn.site.deviceName";
+    private static final String SK_OPENVPN_SITE_CLIENT_NETWORK = "openvpn.site.clientNetwork";
+    private static final String SK_OPENVPN_SITE_CLIENT_MASK = "openvpn.site.clientMask";
+    private static final String SK_OPENVPN_SITE_KEEPALIVE_INTERVAL = "openvpn.site.keepaliveInterval";
+    private static final String SK_OPENVPN_SITE_KEEPALIVE_TIMEOUT = "openvpn.site.keepaliveTimeout";
+
+    public OpenVpnSiteSettings() {
+    }
+
+    public OpenVpnSiteSettings(Settings settings) {
+        listenIp = settings.get(SK_OPENVPN_SITE_LISTEN_IP, "0.0.0.0");
+        listenPort = settings.getInt(SK_OPENVPN_SITE_LISTEN_PORT, 1194);
+        listenProtocol = settings.getEnum(SK_OPENVPN_SITE_LISTEN_PROTOCOL, TransportProtocol.UDP);
+        mtuTest = settings.getBoolean(SK_OPENVPN_SITE_MTU_TEST, true);
+        remote = settings.get(SK_OPENVPN_SITE_REMOTE, NetUtils.myHostname());
+        deviceType = settings.get(SK_OPENVPN_SITE_DEVICE_TYPE, "tun");
+        deviceName = settings.get(SK_OPENVPN_SITE_DEVICE_NAME, "arachne-site");
+        clientNetwork = settings.get(SK_OPENVPN_SITE_CLIENT_NETWORK, "192.168.100.0");
+        clientMask = settings.getInt(SK_OPENVPN_SITE_CLIENT_MASK, 24);
+        keepaliveInterval = settings.getInt(SK_OPENVPN_SITE_KEEPALIVE_INTERVAL, 10);
+        keepaliveTimeout = settings.getInt(SK_OPENVPN_SITE_KEEPALIVE_TIMEOUT, 60);
+    }
+
+    public void save(Settings settings) {
+        settings.put(SK_OPENVPN_SITE_LISTEN_IP, listenIp);
+        settings.put(SK_OPENVPN_SITE_LISTEN_PORT, listenPort);
+        settings.put(SK_OPENVPN_SITE_LISTEN_PROTOCOL, listenProtocol);
+        settings.put(SK_OPENVPN_SITE_MTU_TEST, mtuTest);
+        settings.put(SK_OPENVPN_SITE_REMOTE, remote);
+        settings.put(SK_OPENVPN_SITE_DEVICE_TYPE, deviceType);
+        settings.put(SK_OPENVPN_SITE_DEVICE_NAME, deviceName);
+        settings.put(SK_OPENVPN_SITE_CLIENT_NETWORK, clientNetwork);
+        settings.put(SK_OPENVPN_SITE_CLIENT_MASK, clientMask);
+        settings.put(SK_OPENVPN_SITE_KEEPALIVE_INTERVAL, keepaliveInterval);
+        settings.put(SK_OPENVPN_SITE_KEEPALIVE_TIMEOUT, keepaliveTimeout);
+    }
 
     public VpnSite addVpnSite() {
         int maxId = vpnSites.isEmpty()
--- src/main/java/at/nieslony/arachne/openvpn/OpenVpnSiteView.java
+++ src/main/java/at/nieslony/arachne/openvpn/OpenVpnSiteView.java
@@ -5,10 +5,32 @@
 package at.nieslony.arachne.openvpn;
 
 import at.nieslony.arachne.ViewTemplate;
+import at.nieslony.arachne.settings.Settings;
+import at.nieslony.arachne.utils.net.NetMask;
+import at.nieslony.arachne.utils.net.NicInfo;
+import at.nieslony.arachne.utils.net.NicUtils;
+import at.nieslony.arachne.utils.net.TransportProtocol;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.tabs.TabSheet;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  *
@@ -19,6 +41,202 @@ import jakarta.annotation.security.RolesAllowed;
 @RolesAllowed("ADMIN")
 public class OpenVpnSiteView extends VerticalLayout {
 
-    public OpenVpnSiteView() {
+    private final Binder<OpenVpnSiteSettings> binder;
+    private final OpenVpnSiteSettings openVpnSiteSettings;
+
+    public OpenVpnSiteView(Settings settings) {
+        binder = new Binder<>(OpenVpnSiteSettings.class);
+        openVpnSiteSettings = new OpenVpnSiteSettings(settings);
+
+        TabSheet tabs = new TabSheet();
+        tabs.add("Basics", createBasicsPage());
+        tabs.add("Clients", createClientsPage());
+
+        Button saveButton = new Button("Save");
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        add(tabs, saveButton);
+
+        binder.setBean(openVpnSiteSettings);
+        binder.validate();
+    }
+
+    private Component createBasicsPage() {
+        Select<NicInfo> ipAddresse = new Select<>();
+        ipAddresse.setItems(NicUtils.findAllNics());
+        ipAddresse.setLabel("Listen on");
+        ipAddresse.setWidth(20, Unit.EM);
+        binder.forField(ipAddresse)
+                .bind(
+                        (s) -> {
+                            return NicUtils.findNicByIp(s.getListenIp());
+                        },
+                        (s, v) -> {
+                            s.setListenIp(v.getIpAddress());
+                        });
+
+        IntegerField port = new IntegerField("Port");
+        port.setMin(1);
+        port.setMax(65534);
+        port.setStepButtonsVisible(true);
+        port.setValueChangeMode(ValueChangeMode.EAGER);
+        port.setWidth(8, Unit.EM);
+        binder.bind(port, "listenPort");
+
+        Select<TransportProtocol> protocol = new Select<>();
+        protocol.setItems(TransportProtocol.values());
+        protocol.setLabel("Protocol");
+        protocol.setWidth(8, Unit.EM);
+        binder.bind(protocol, "listenProtocol");
+
+        HorizontalLayout listenLayout = new HorizontalLayout();
+        listenLayout.add(
+                ipAddresse,
+                new Text(":"),
+                port,
+                new Text("/"),
+                protocol);
+        listenLayout.setAlignItems(Alignment.BASELINE);
+        listenLayout.setFlexGrow(1, ipAddresse);
+
+        TextField connectToHost = new TextField("Connect to host");
+        connectToHost.setValueChangeMode(ValueChangeMode.EAGER);
+        binder.bind(
+                connectToHost,
+                OpenVpnSiteSettings::getRemote,
+                OpenVpnSiteSettings::setRemote
+        );
+
+        Select<String> interfaceType = new Select<>();
+        interfaceType.setItems("tun", "tap");
+        interfaceType.setLabel("Interface Type");
+        interfaceType.setWidth(8, Unit.EM);
+        binder.bind(
+                interfaceType,
+                OpenVpnSiteSettings::getDeviceType,
+                OpenVpnSiteSettings::setDeviceType
+        );
+
+        TextField interfaceName = new TextField("Interface Name");
+        interfaceName.setValueChangeMode(ValueChangeMode.EAGER);
+        binder.bind(
+                interfaceName,
+                OpenVpnSiteSettings::getDeviceName,
+                OpenVpnSiteSettings::setDeviceName
+        );
+
+        HorizontalLayout interfaceLayout = new HorizontalLayout();
+        interfaceLayout.add(interfaceType, interfaceName);
+        interfaceLayout.setFlexGrow(1, interfaceName);
+
+        TextField clientNetwork = new TextField("Client Network");
+        clientNetwork.setValueChangeMode(ValueChangeMode.EAGER);
+        binder.bind(
+                clientNetwork,
+                OpenVpnSiteSettings::getClientNetwork,
+                OpenVpnSiteSettings::setClientNetwork
+        );
+
+        Select<NetMask> clientMask = new Select<>();
+        clientMask.setItems(
+                IntStream
+                        .range(1, 32)
+                        .boxed()
+                        .map(i -> new NetMask(i))
+                        .collect(Collectors.toList())
+        );
+        clientMask.setLabel("Subnet Mask");
+        binder.forField(clientMask)
+                .bind(
+                        (source) -> {
+                            int mask = source.getClientMask();
+                            return new NetMask(mask);
+                        },
+                        (s, v) -> {
+                            s.setClientMask(v.getBits());
+                        }
+                );
+
+        HorizontalLayout clientNetLayout = new HorizontalLayout();
+        clientNetLayout.add(clientNetwork, clientMask);
+        clientNetLayout.setFlexGrow(1, clientNetwork, clientMask);
+
+        IntegerField keepaliveInterval = new IntegerField("Keepalive Interval");
+        Div suffix;
+        suffix = new Div();
+        suffix.setText("seconds");
+        keepaliveInterval.setSuffixComponent(suffix);
+        keepaliveInterval.setMin(1);
+        keepaliveInterval.setStepButtonsVisible(true);
+        keepaliveInterval.setWidth(12, Unit.EM);
+        keepaliveInterval.setValueChangeMode(ValueChangeMode.EAGER);
+        binder.bind(
+                keepaliveInterval,
+                OpenVpnSiteSettings::getKeepaliveInterval,
+                OpenVpnSiteSettings::setKeepaliveInterval
+        );
+
+        IntegerField keepaliveTimeout = new IntegerField("Keepalive timeout");
+        suffix = new Div();
+        suffix.setText("seconds");
+        keepaliveTimeout.setSuffixComponent(suffix);
+        keepaliveTimeout.setMin(1);
+        keepaliveTimeout.setStepButtonsVisible(true);
+        keepaliveTimeout.setWidth(12, Unit.EM);
+        keepaliveInterval.setValueChangeMode(ValueChangeMode.EAGER);
+        binder.bind(
+                keepaliveTimeout,
+                OpenVpnSiteSettings::getKeepaliveTimeout,
+                OpenVpnSiteSettings::setKeepaliveTimeout
+        );
+
+        HorizontalLayout keepaliveLayout = new HorizontalLayout();
+        keepaliveLayout.add(keepaliveInterval, keepaliveTimeout);
+
+        Checkbox mtuTestField = new Checkbox("MTU Test");
+        binder.bind(
+                mtuTestField,
+                OpenVpnSiteSettings::getMtuTest,
+                OpenVpnSiteSettings::setMtuTest
+        );
+
+        FormLayout layout = new FormLayout();
+        layout.add(
+                listenLayout,
+                connectToHost,
+                interfaceLayout,
+                clientNetLayout,
+                keepaliveLayout,
+                mtuTestField
+        );
+
+        return layout;
+    }
+
+    private Component createClientsPage() {
+        VerticalLayout layout = new VerticalLayout();
+
+        Select<OpenVpnSiteSettings.VpnSite> sites = new Select<>();
+        sites.setLabel("Sites");
+        sites.setWidthFull();
+
+        Button renameButton = new Button("Rename...");
+
+        Button deleteButton = new Button("Delete");
+
+        Button addButton = new Button("Add...");
+
+        HorizontalLayout buttonLayout = new HorizontalLayout(
+                renameButton,
+                deleteButton,
+                addButton
+        );
+
+        layout.add(
+                sites,
+                buttonLayout
+        );
+
+        return layout;
     }
 }
