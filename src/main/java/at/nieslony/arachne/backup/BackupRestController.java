a1e2445f099efd5e54daedbeaf7f8690df643942
Claas Nieslony <bb@nieslony.at>
2023-09-09 00:54:42 +0200

    Add package at/nieslony/arachne/backup

---
+++ src/main/java/at/nieslony/arachne/backup/BackupRestController.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package at.nieslony.arachne.backup;
+
+import at.nieslony.arachne.firewall.FirewallRuleRepository;
+import at.nieslony.arachne.pki.CertificateRepository;
+import at.nieslony.arachne.pki.KeyRepository;
+import at.nieslony.arachne.roles.RoleRuleRepository;
+import at.nieslony.arachne.settings.SettingsRepository;
+import at.nieslony.arachne.tasks.RecurringTasksRepository;
+import at.nieslony.arachne.tasks.TaskRepository;
+import at.nieslony.arachne.users.UserRepository;
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author claas
+ */
+@RestController
+public class BackupRestController {
+
+    @Autowired
+    FirewallRuleRepository firewallRuleRepository;
+    @Autowired
+    CertificateRepository certificateRepository;
+    @Autowired
+    KeyRepository keyRepository;
+    @Autowired
+    RoleRuleRepository roleRuleRepository;
+    @Autowired
+    SettingsRepository settingsRepository;
+    @Autowired
+    RecurringTasksRepository recurringTasksRepository;
+    @Autowired
+    TaskRepository taskRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/api/backup")
+    @RolesAllowed(value = {"ADMIN", "BACKUP"})
+    public Backup getBackup() {
+        return new Backup(
+                firewallRuleRepository,
+                certificateRepository,
+                keyRepository,
+                roleRuleRepository,
+                settingsRepository,
+                recurringTasksRepository,
+                taskRepository,
+                userRepository
+        );
+    }
+}
