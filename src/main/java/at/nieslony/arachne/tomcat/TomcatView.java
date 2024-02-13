bb7c034f7f93312d6e77dd7c61eafcf725807dec
Claas Nieslony <github@nieslony.at>
2024-02-13 17:38:47 +0100

    TomcatView: GUI improvements

--- src/main/java/at/nieslony/arachne/tomcat/TomcatView.java
+++ src/main/java/at/nieslony/arachne/tomcat/TomcatView.java
@@ -19,9 +19,13 @@ package at.nieslony.arachne.tomcat;
 import at.nieslony.arachne.ViewTemplate;
 import at.nieslony.arachne.settings.Settings;
 import at.nieslony.arachne.settings.SettingsException;
+import at.nieslony.arachne.utils.ShowNotification;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
@@ -30,6 +34,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 import jakarta.annotation.security.RolesAllowed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,35 +50,57 @@ public class TomcatView extends VerticalLayout {
 
     private static final Logger logger = LoggerFactory.getLogger(TomcatView.class);
 
-    private final Settings settings;
-    private final TomcatService tomcatService;
-
     public TomcatView(Settings settings, TomcatService tomcatService) {
-        this.settings = settings;
-        this.tomcatService = tomcatService;
         Binder<TomcatSettings> binder = new Binder<>(TomcatSettings.class);
+        TomcatSettings tomcatSettings = settings.getSettings(TomcatSettings.class);
 
         Checkbox enableAjpField = new Checkbox("Enable AJP Connector");
         enableAjpField.setValue(true);
 
         IntegerField ajpPortField = new IntegerField("Port");
-
-        Checkbox enableAjpSecretField = new Checkbox("Enable AJP Secret");
-        enableAjpSecretField.setValue(true);
-        PasswordField ajpSecretField = new PasswordField();
-        HorizontalLayout secretLayout = new HorizontalLayout();
-        secretLayout.add(enableAjpSecretField, ajpSecretField);
-        secretLayout.setAlignItems(Alignment.CENTER);
+        ajpPortField.setMin(1);
+        ajpPortField.setMax(65535);
+        ajpPortField.setStepButtonsVisible(true);
 
         TextField ajpLocationField = new TextField("Outsite Location");
+        ajpLocationField.setClearButtonVisible(true);
+
+        Checkbox enableAjpSecretField = new Checkbox("Enable AJP Secret");
+        enableAjpField.addClassNames(LumoUtility.FlexWrap.NOWRAP);
+        enableAjpSecretField.setValue(true);
+        PasswordField ajpSecretField = new PasswordField();
+        Button createSecret = new Button(
+                "Create new Secret",
+                (e) -> {
+                    ajpSecretField.setValue(tomcatSettings.createSecret());
+                }
+        );
+        HorizontalLayout secretLayout = new HorizontalLayout();
+        secretLayout.add(
+                enableAjpSecretField,
+                ajpSecretField,
+                createSecret
+        );
+        secretLayout.setAlignItems(FlexComponent.Alignment.BASELINE);
+        secretLayout.setFlexGrow(1, ajpSecretField);
 
         Button saveAndRestartButton = new Button(
-                "Save and Restart Arachne",
+                "Save",
                 e -> {
                     try {
-                        TomcatSettings tomcatSettings = binder.getBean();
                         tomcatSettings.save(settings);
                         tomcatService.saveApacheConfig();
+                        ShowNotification.info(
+                                "Tomcat Configuration Saved",
+                                """
+                                You can also find a configuration for Apache
+                                HTTP Server at %s.
+
+                                Please copy or symlink it to your apache
+                                configuration folder e.g. /etc/httpt/conf.d and
+                                restart apache.
+                                """.formatted(tomcatService.getApacheConfigFileName())
+                        );
                     } catch (SettingsException ex) {
                         logger.error("Cannot save tomcat settings: " + ex.getMessage());
                     }
@@ -101,11 +128,13 @@ public class TomcatView extends VerticalLayout {
                         enableAjpSecretField.setEnabled(true);
                         ajpSecretField.setEnabled(enableAjpSecretField.getValue());
                         ajpLocationField.setEnabled(true);
+                        createSecret.setEnabled(true);
                     } else {
                         ajpPortField.setEnabled(false);
                         enableAjpSecretField.setEnabled(false);
                         ajpSecretField.setEnabled(false);
                         ajpLocationField.setEnabled(false);
+                        createSecret.setEnabled(false);
                     }
                 });
         enableAjpSecretField.addValueChangeListener(
@@ -113,15 +142,20 @@ public class TomcatView extends VerticalLayout {
                     ajpSecretField.setEnabled(e.getValue());
                 });
 
-        TomcatSettings tomcatSettings = settings.getSettings(TomcatSettings.class);
         binder.setBean(tomcatSettings);
         binder.validate();
 
+        FormLayout settingsLayout = new FormLayout(
+                ajpPortField,
+                ajpLocationField,
+                secretLayout
+        );
+        settingsLayout.setMaxWidth(100, Unit.EX);
+        settingsLayout.setColspan(secretLayout, 2);
+
         add(
                 enableAjpField,
-                ajpPortField,
-                secretLayout,
-                ajpLocationField,
+                settingsLayout,
                 saveAndRestartButton
         );
     }
