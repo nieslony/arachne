93c3e6b5d5aaa2325587fdc9026b40f24b59235f
Claas Nieslony <github@nieslony.at>
2024-02-20 14:03:45 +0100

    Remove the arachne

--- src/main/java/at/nieslony/arachne/Arachne.java
+++ src/main/java/at/nieslony/arachne/Arachne.java
@@ -4,7 +4,6 @@
 package at.nieslony.arachne;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,7 +18,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("at.nieslony.arachne")
 @EntityScan("at.nieslony.arachne")
 @SpringBootApplication
-@Theme(value = "arachne")
 public class Arachne implements AppShellConfigurator {
 
     private static ConfigurableApplicationContext context;
