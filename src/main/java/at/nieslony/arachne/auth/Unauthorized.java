1e38b7879a9963121d4ed3deded3302e9c57f8d2
Claas Nieslony <github@nieslony.at>
2024-03-29 09:38:50 +0100

    Add form flag

---
+++ src/main/java/at/nieslony/arachne/auth/Unauthorized.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2024 claas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.nieslony.arachne.auth;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author claas
+ */
+@RestController
+public class Unauthorized {
+
+    @RequestMapping("/unauthorized")
+    @AnonymousAllowed
+    public String unauthorized() {
+        return """
+               <html>
+               <head>
+               </head>
+
+               <body>
+               Unauthorized
+               </body>
+               </html>
+               """;
+    }
+}
