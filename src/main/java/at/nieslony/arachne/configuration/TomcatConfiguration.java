ebf888e23d18457a2000bb9831f754041dd949ee
Claas Nieslony <github@nieslony.at>
2024-04-05 13:06:49 +0200

    Enable SSL on port 8443, create RSA key and certificate if not exists

--- src/main/java/at/nieslony/arachne/configuration/TomcatConfiguration.java
+++ src/main/java/at/nieslony/arachne/configuration/TomcatConfiguration.java
@@ -7,10 +7,40 @@ package at.nieslony.arachne.configuration;
 import at.nieslony.arachne.auth.PreAuthSettings;
 import at.nieslony.arachne.settings.Settings;
 import at.nieslony.arachne.tomcat.TomcatSettings;
+import at.nieslony.arachne.utils.net.NetUtils;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import org.apache.catalina.connector.Connector;
 import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.GeneralName;
+import org.bouncycastle.asn1.x509.GeneralNames;
+import org.bouncycastle.cert.CertIOException;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.Ssl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,14 +51,116 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TomcatConfiguration {
 
+    private static final org.slf4j.Logger logger
+            = LoggerFactory.getLogger(TomcatConfiguration.class);
+
     @Autowired
     Settings settings;
+
+    @Value("${some.key:true}")
+    boolean enableTomcatSsl;
+
+    @Value("${tomcatCertPath:${arachneConfigDir}/server.crt}")
+    String tomcatCertPath;
+
+    @Value("${tomcatKeyPath:${arachneConfigDir}/server.key}")
+    String tomcatKeyPath;
+
+    private KeyPair createSslKey() {
+
+        try {
+            logger.info("Creating RSA key " + tomcatKeyPath);
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(
+                    "RSA"
+            );
+            keyPairGenerator.initialize(2048);
+            KeyPair keyPair = keyPairGenerator.genKeyPair();
+            try (JcaPEMWriter pemWriter = new JcaPEMWriter(
+                    new FileWriter(tomcatKeyPath))) {
+                pemWriter.writeObject(keyPair.getPrivate());
+            }
+            return keyPair;
+        } catch (NoSuchAlgorithmException | IOException ex) {
+            logger.error("Cannot write key to %s: %s".formatted(
+                    tomcatKeyPath, ex.getMessage()
+            ));
+            return null;
+        }
+    }
+
+    // https://gamlor.info/posts-output/2019-10-29-java-create-certs-bouncy/en/
+    private void createSslCertificate(KeyPair keyPair) {
+        X509Certificate cert;
+
+        String myHostname = NetUtils.myHostname();
+        X500Name subject = new X500Name("CN=" + myHostname);
+        BigInteger serialNumber = BigInteger.valueOf(System.currentTimeMillis());
+        Instant validFrom = Instant.now();
+        Instant validUntil = validFrom.plus(10 * 360, ChronoUnit.DAYS);
+        JcaX509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
+                subject,
+                serialNumber,
+                Date.from(validFrom),
+                Date.from(validUntil),
+                subject,
+                keyPair.getPublic()
+        );
+        try {
+            certBuilder.addExtension(
+                    Extension.subjectAlternativeName,
+                    false,
+                    new GeneralNames(new GeneralName[]{
+                new GeneralName(GeneralName.dNSName, NetUtils.myDomain()),
+                new GeneralName(GeneralName.dNSName, NetUtils.myHostname()),
+                new GeneralName(GeneralName.dNSName, "localhost"),}
+                    )
+            );
+            ContentSigner certSigner = new JcaContentSignerBuilder("SHA256WithRSA")
+                    .build(keyPair.getPrivate());
+            X509CertificateHolder certHolder = certBuilder.build(certSigner);
+            cert = new JcaX509CertificateConverter()
+                    .getCertificate(certHolder);
+        } catch (CertIOException | OperatorCreationException | CertificateException ex) {
+            logger.error("Cannor create certificate: " + ex.getMessage());
+            return;
+        }
+        try (JcaPEMWriter pemWriter
+                = new JcaPEMWriter(new FileWriter(tomcatCertPath))) {
+            pemWriter.writeObject(cert);
+        } catch (IOException ex) {
+            logger.error("Cannot write certificate ti %s: %s"
+                    .formatted(tomcatCertPath, ex.getMessage())
+            );
+        }
+    }
 
     @Bean
     public TomcatServletWebServerFactory servletContainer() {
         TomcatSettings tomcatSettings = settings.getSettings(TomcatSettings.class);
         PreAuthSettings preAuthSettings = settings.getSettings(PreAuthSettings.class);
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+
+        if (enableTomcatSsl) {
+            logger.info("Enabling Tomcat SSL");
+            Path keyPath = Paths.get(tomcatKeyPath);
+            if (!Files.exists(keyPath)) {
+                KeyPair keyPair = createSslKey();
+                createSslCertificate(keyPair);
+            }
+
+            Ssl ssl = new Ssl();
+            ssl.setCertificate(tomcatCertPath);
+            ssl.setCertificatePrivateKey(tomcatKeyPath);
+
+            tomcat.setSsl(ssl);
+            tomcat.setPort(8443);
+
+            Connector httpConnector = new Connector();
+            httpConnector.setPort(8080);
+            tomcat.addAdditionalTomcatConnectors(httpConnector);
+        } else {
+            logger.info("Tomcat SSL is disabled");
+        }
 
         if (tomcatSettings.isEnableAjpConnector()) {
             Connector ajpConnector = new Connector("AJP/1.3");
