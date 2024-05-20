37606df7ba93d0669a6c4d54f26f83f51ac9479a
Claas Nieslony <github@nieslony.at>
2024-05-20 11:18:34 +0200

    Removed unused attributes, add NM con uuid

--- src/main/java/at/nieslony/arachne/openvpn/VpnSite.java
+++ src/main/java/at/nieslony/arachne/openvpn/VpnSite.java
@@ -5,6 +5,7 @@
 package at.nieslony.arachne.openvpn;
 
 import at.nieslony.arachne.utils.net.NetUtils;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -52,18 +53,20 @@ public class VpnSite {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    // Basics
+    @Column(nullable = false, unique = true)
     private String name;
     private String description;
     @Builder.Default
     private boolean defaultSite = false;
+    // Connection
     @Builder.Default
     private String remoteHost = "www.example.com";
     @Builder.Default
     private SiteVerification siteVerification = SiteVerification.DNS;
     @Builder.Default
     private List<String> ipWhiteList = new LinkedList<>();
-    private String sshUser;
-    private String sshPrivateKey;
+    // DNS
     @Builder.Default
     private boolean inheritDnsServers = true;
     @Builder.Default
@@ -72,6 +75,7 @@ public class VpnSite {
     private boolean inheritPushDomains = true;
     @Builder.Default
     private List<String> pushSearchDomains = Arrays.asList(NetUtils.myDomain());
+    // Routing
     @Builder.Default
     private boolean inheritPushRoutes = true;
     @Builder.Default
@@ -80,6 +84,7 @@ public class VpnSite {
     boolean inheritRouteInternetThroughVpn = true;
     @Builder.Default
     private boolean routeInternetThroughVpn = false;
+    private String networkManagerConnectionUuid;
 
     public VpnSite() {
     }
