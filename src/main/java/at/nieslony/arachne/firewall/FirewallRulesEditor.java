945d4955e179219a9e79494fb6c9caca1373004b
Claas Nieslony <github@nieslony.at>
2024-08-08 11:44:44 +0200

    Move UI components to new package

--- src/main/java/at/nieslony/arachne/firewall/FirewallRulesEditor.java
+++ src/main/java/at/nieslony/arachne/firewall/FirewallRulesEditor.java
@@ -6,8 +6,8 @@ package at.nieslony.arachne.firewall;
 
 import at.nieslony.arachne.ldap.LdapSettings;
 import at.nieslony.arachne.usermatcher.UserMatcherCollector;
-import at.nieslony.arachne.utils.MagicEditableListBox;
-import at.nieslony.arachne.utils.YesNoIcon;
+import at.nieslony.arachne.utils.components.MagicEditableListBox;
+import at.nieslony.arachne.utils.components.YesNoIcon;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
--- src/main/java/at/nieslony/arachne/utils/components/YesNoIcon.java
+++ src/main/java/at/nieslony/arachne/utils/components/YesNoIcon.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package at.nieslony.arachne.utils;
+package at.nieslony.arachne.utils.components;
 
 import com.vaadin.flow.component.AbstractCompositeField;
 import com.vaadin.flow.component.button.Button;
