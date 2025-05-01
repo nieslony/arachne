2f0b371a3770c88ff0610e7369edc9a462bfd2a5
Claas Nieslony <github@nieslony.at>
2025-05-01 21:25:55 +0200

    Fix: @Push(transport = Transport.LONG_POLLING, value = PushMode.AUTOMATIC) is required for AJP

--- src/main/java/at/nieslony/arachne/Arachne.java
+++ src/main/java/at/nieslony/arachne/Arachne.java
@@ -6,6 +6,8 @@ package at.nieslony.arachne;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.AppShellSettings;
+import com.vaadin.flow.shared.communication.PushMode;
+import com.vaadin.flow.shared.ui.Transport;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
@@ -22,7 +24,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("at.nieslony.arachne")
 @Theme(value = "arachne")
 @SpringBootApplication
-@Push
+@Push(transport = Transport.LONG_POLLING, value = PushMode.AUTOMATIC)
 public class Arachne implements AppShellConfigurator {
 
     private static ConfigurableApplicationContext context;
