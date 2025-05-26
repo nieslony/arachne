88e720470a7f6b717ea0dde910af77a18a9844b9
Claas Nieslony <github@nieslony.at>
2025-05-26 14:48:43 +0200

    Automatic commit of package [arachne] release [1.4.0.git_2505261448_ca3fa13-1].
    Created by command:
    
    /usr/bin/tito tag --use-version=1.4.0.git_2505261448_ca3fa13

--- src/main/java/at/nieslony/arachne/ArachneVersionBase.java
+++ src/main/java/at/nieslony/arachne/ArachneVersionBase.java
@@ -2,5 +2,5 @@ package at.nieslony.arachne;
 
 public class ArachneVersionBase {
 
-    public static final String ARACHNE_VERSION = "1.4.0.git_2505261224_f7aea1e";
+    public static final String ARACHNE_VERSION = "1.4.0.git_2505261448_ca3fa13";
 }
