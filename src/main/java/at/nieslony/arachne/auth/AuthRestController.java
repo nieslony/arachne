b5ac9e3cd62abad28beb4554359eb33780998f71
Claas Nieslony <github@nieslony.at>
2026-07-01 20:40:32 +0200

    Remove OTP verification

--- src/main/java/at/nieslony/arachne/auth/AuthRestController.java
+++ src/main/java/at/nieslony/arachne/auth/AuthRestController.java
@@ -20,8 +20,8 @@ import at.nieslony.arachne.apiindex.ShowApiDetails;
 import at.nieslony.arachne.auth.token.TokenController;
 import at.nieslony.arachne.openvpn.OpenVpnController;
 import at.nieslony.arachne.users.ArachneUserDetails;
-import at.nieslony.arachne.users.UserModel;
 import at.nieslony.arachne.users.UserRepository;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 import jakarta.annotation.security.RolesAllowed;
 import java.util.Calendar;
 import java.util.Date;
@@ -29,14 +29,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -46,6 +44,7 @@ import org.springframework.web.server.ResponseStatusException;
  * @author claas
  */
 @RestController
+
 @Slf4j
 public class AuthRestController {
 
@@ -129,38 +128,27 @@ public class AuthRestController {
         return validUntilCal.getTime();
     }
 
+    @GetMapping("/api/error/{statusCode}")
+    @AnonymousAllowed
+    public String error(@PathVariable Integer statusCode) {
+        try {
+            HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
+            return "Error %d: %s\n".formatted(
+                    httpStatus.value(),
+                    httpStatus.getReasonPhrase()
+            );
+        } catch (IllegalArgumentException ex) {
+            return "";
+        }
+    }
+
     @GetMapping("/api/login")
     @RolesAllowed(value = {"USER", "ADMIN"})
+
     public AuthResult login(
             @RequestParam(required = false, defaultValue = "10min") String validTime,
-            @AuthenticationPrincipal UserDetails userDetails,
-            @RequestBody(required = false) String body
+            @AuthenticationPrincipal UserDetails userDetails
     ) {
-        UserModel user = userRepository.findByUsername(userDetails.getUsername());
-
-        if (openVpnController.isOtpRequired(user)) {
-            log.info("Verifying OTP");
-            try {
-                if (body == null) {
-                    log.error("OTP expected");
-                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-                }
-                log.debug("Parsing json: " + body);
-                JSONObject json = new JSONObject(body);
-                String otp = json.getString("otp");
-                if (!totpController.validateTotp(otp, user)) {
-                    log.error("Invalid TOTP");
-                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-                }
-            } catch (JSONException ex) {
-                log.error("Cannot parse JSON String %s: %s".formatted(body, ex.getMessage()));
-                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-            }
-            log.info("OTP is valid");
-        } else {
-            log.info("No OTP required");
-        }
-
         return getToken(validTime, userDetails);
     }
 
